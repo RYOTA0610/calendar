1e20e4b34282ecfeac1e8ded47deec4de8d430d7
RYOTA <ryota0610ryota@gmail.com>
2025-04-13 17:49:49 +0900

    イベントリスナーの設定

--- develop/index.ts
+++ develop/index.ts
@@ -14,3 +14,26 @@ const elmNext: HTMLElement = document.querySelector(".cal__next")!;
 
 /*日付表示部*/
 const elmDays: HTMLElement = document.querySelector(".cal__days")!;
+
+
+/*イベントハンドラ*/
+
+/*アプリケーションを初期化*/
+const onPageLoad = (): void => { };
+
+/*前月移動処理*/
+const onPrev = (): void => { };
+
+/*翌月移動処理*/
+const onNext = ():void => { };
+
+/*イベントリスナー*/
+
+/*ページの読み込み完了イベント*/
+window.addEventListener("load", onPageLoad);
+
+/*前月移動ナビのクリックイベント*/
+elmPrev.addEventListener("click", onPrev);
+
+/*翌月移動ナビのクリックイベント*/
+elmNext.addEventListener("click", onNext);
