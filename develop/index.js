e23227225e1b6008e1af3a4f84a7673ae44c5a40
RYOTA <ryota0610ryota@gmail.com>
2025-04-16 00:23:56 +0900

    初期画面の実装

--- develop/index.js
+++ develop/index.js
@@ -1,73 +1,54 @@
 /*グローバル変数*/
-
 /*表示中の年月*/
-const currentDate: Date = new Date();
-
+const currentDate = new Date();
 /*タイトル表示部*/
-const elmTitle: HTMLElement = document.querySelector(".cal__title")!;
-
+const elmTitle = document.querySelector(".cal__title");
 /*前月移動ナビ*/
-const elmPrev: HTMLElement = document.querySelector(".cal__prev")!;
-
+const elmPrev = document.querySelector(".cal__prev");
 /*翌月移動ナビ*/
-const elmNext: HTMLElement = document.querySelector(".cal__next")!;
-
+const elmNext = document.querySelector(".cal__next");
 /*日付表示部*/
-const elmDays: HTMLElement = document.querySelector(".cal__days")!;
-
-
+const elmDays = document.querySelector(".cal__days");
 /*イベントハンドラ*/
-
 /*アプリケーションを初期化*/
-const onPageLoad = (): void => {
+const onPageLoad = () => {
     //描画を更新
     updateView(currentDate);
 };
-
 /*前月移動処理*/
-const onPrev = (): void => { };
-
+const onPrev = () => { };
 /*翌月移動処理*/
-const onNext = ():void => { };
-
+const onNext = () => { };
 /*イベントリスナー*/
-
 /*ページの読み込み完了イベント*/
 window.addEventListener("load", onPageLoad);
-
 /*前月移動ナビのクリックイベント*/
 elmPrev.addEventListener("click", onPrev);
-
 /*翌月移動ナビのクリックイベント*/
 elmNext.addEventListener("click", onNext);
-
-
 /*ユーザー定義関数*/
-
 /*描画更新*/
-function updateView(date: Date): void {
+function updateView(date) {
     //タイトル表示部の更新
     updateTitle(date);
     //日付表示部の更新
     updateDays(date);
 }
-
-function updateTitle(date: Date): void {
-    const title: string = date.getFullYear().toString() + "年" + (date.getMonth() + 1).toString().padStart(2, "0") + "月";
+function updateTitle(date) {
+    const title = date.getFullYear().toString() + "年" + (date.getMonth() + 1).toString().padStart(2, "0") + "月";
     elmTitle.innerHTML = title;
 }
-
-function updateDays(date: Date): void {
+function updateDays(date) {
     //配列を宣言
-    const dateList: number[] = [];
-    const classList: string[] = [];
+    const dateList = [];
+    const classList = [];
     //日付の表示に必要な情報を求める
-    const thisDays: number = getMonthDays(date);
-    const prevDays: number = getFirstDayOfWeek(date);
-    const prevLastDate: number = getPrevMonthDays(date);
-    const rows: number = Math.ceil((thisDays + prevDays) / 7);
+    const thisDays = getMonthDays(date);
+    const prevDays = getFirstDayOfWeek(date);
+    const prevLastDate = getPrevMonthDays(date);
+    const rows = Math.ceil((thisDays + prevDays) / 7);
     //セルのデータを配列に格納する
-    for (let i: number = 0; i < rows * 7; i++) {
+    for (let i = 0; i < rows * 7; i++) {
         if (i < prevDays) {
             dateList.push(prevLastDate + prevDays + 1 + i);
             classList.push("cal__days cal__day--prev");
@@ -85,14 +66,13 @@ function updateDays(date: Date): void {
             }
         }
         else {
-                dateList.push(i - (prevDays + thisDays) + 1);
-                classList.push("cal__days cal__day--next");
-
-            }
+            dateList.push(i - (prevDays + thisDays) + 1);
+            classList.push("cal__days cal__day--next");
         }
+    }
     //日付表示部のHTMLを編集する
-    let html: string = "";
-    for (let i: number = 0; i < rows * 7; i++) {
+    let html = "";
+    for (let i = 0; i < rows * 7; i++) {
         if (i % 7 === 0) {
             html += "<tr>";
         }
@@ -104,34 +84,20 @@ function updateDays(date: Date): void {
     //日付表示部のHTMLを更新する
     elmDays.innerHTML = html;
 }
-
-function getMonthDays(date: Date): number {
-    const lastDay: Date = new Date(
-        date.getFullYear(),
-        date.getMonth(),
-        date.getDate()
-    );
+function getMonthDays(date) {
+    const lastDay = new Date(date.getFullYear(), date.getMonth(), date.getDate());
     lastDay.setMonth(lastDay.getMonth() + 1);
     lastDay.setDate(0);
-    const days: number = lastDay.getDate();
+    const days = lastDay.getDate();
     return days;
 }
-
-function getFirstDayOfWeek(date: Date): number {
-    const firstDay: Date = new Date(
-        date.getFullYear(),
-        date.getMonth(),
-        1
-    );
-    const day: number = firstDay.getDay();
+function getFirstDayOfWeek(date) {
+    const firstDay = new Date(date.getFullYear(), date.getMonth(), 1);
+    const day = firstDay.getDay();
     return day;
 }
-
-function getPrevMonthDays(date: Date): number {
-    const prevMonth: Date = new Date(
-        date.getFullYear(),
-        date.getMonth() - 1,
-    );
-    const days: number = getMonthDays(prevMonth)
+function getPrevMonthDays(date) {
+    const prevMonth = new Date(date.getFullYear(), date.getMonth() - 1);
+    const days = getMonthDays(prevMonth);
     return days;
 }
