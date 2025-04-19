4224bc9a49569c4f22500db519bd5376b9c6feea
RYOTA <ryota0610ryota@gmail.com>
2025-04-19 20:56:11 +0900

    リファクタリング

--- docs/index.ts
+++ docs/index.ts
@@ -1,60 +1,30 @@
-/*グローバル変数*/
-
-/*表示中の年月*/
 const currentDate: Date = new Date();
-
-/*タイトル表示部*/
 const elmTitle: HTMLElement = document.querySelector(".cal__title")!;
-
-/*前月移動ナビ*/
 const elmPrev: HTMLElement = document.querySelector(".cal__prev")!;
-
-/*翌月移動ナビ*/
 const elmNext: HTMLElement = document.querySelector(".cal__next")!;
-
-/*日付表示部*/
 const elmDays: HTMLElement = document.querySelector(".cal__days")!;
 
-
-/*イベントハンドラ*/
-
-/*アプリケーションを初期化*/
 const onPageLoad = (): void => {
-    //描画を更新
     updateView(currentDate);
 };
 
-/*前月移動処理*/
 const onPrev = (): void => {
   currentDate.setMonth(currentDate.getMonth() - 1);
   updateView(currentDate);
 };
 
-/*翌月移動処理*/
 const onNext = (): void => {
   currentDate.setMonth(currentDate.getMonth() + 1);
   updateView(currentDate);
 };
 
-/*イベントリスナー*/
-
-/*ページの読み込み完了イベント*/
 window.addEventListener("load", onPageLoad);
-
-/*前月移動ナビのクリックイベント*/
 elmPrev.addEventListener("click", onPrev);
-
-/*翌月移動ナビのクリックイベント*/
 elmNext.addEventListener("click", onNext);
 
-
-/*ユーザー定義関数*/
-
-/*描画更新*/
 function updateView(date: Date): void {
-    //タイトル表示部の更新
     updateTitle(date);
-    //日付表示部の更新
+
     updateDays(date);
 }
 
@@ -64,15 +34,12 @@ function updateTitle(date: Date): void {
 }
 
 function updateDays(date: Date): void {
-    //配列を宣言
     const dateList: number[] = [];
     const classList: string[] = [];
-    //日付の表示に必要な情報を求める
     const thisDays: number = getMonthDays(date);
     const prevDays: number = getFirstDayOfWeek(date);
     const prevLastDate: number = getPrevMonthDays(date);
     const rows: number = Math.ceil((thisDays + prevDays) / 7);
-    //セルのデータを配列に格納する
     for (let i: number = 0; i < rows * 7; i++) {
         if (i < prevDays) {
             dateList.push(prevLastDate - prevDays + 1 + i);
@@ -96,7 +63,6 @@ function updateDays(date: Date): void {
 
             }
         }
-    //日付表示部のHTMLを編集する
     let html: string = "";
     for (let i: number = 0; i < rows * 7; i++) {
         if (i % 7 === 0) {
@@ -107,7 +73,6 @@ function updateDays(date: Date): void {
             html += "</tr>";
         }
     }
-    //日付表示部のHTMLを更新する
     elmDays.innerHTML = html;
 }
 
